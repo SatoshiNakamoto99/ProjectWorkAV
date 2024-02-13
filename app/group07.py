5e4d4b272a487ef2cf6a8a8543a83279a4e83614
priscotrotta00 <priscotrotta00@gmail.com>
2024-02-13 15:17:33 +0100

    tolto commenti

--- app/group07.py
+++ app/group07.py
@@ -1,39 +1,3 @@
-#python app/group07.py --video data/video_atrio_cues/video01.mp4 --configuration config.txt --results results/video_atrio_cues/video01.txt
-#python app/group07.py --video data/video_atrio_cues/video02.mp4 --configuration config.txt --results results/video_atrio_cues/video02.txt
-#python app/group07.py --video data/video_atrio_cues/video03.mp4 --configuration config.txt --results results/video_atrio_cues/video03.txt
-#python app/group07.py --video data/video_atrio_cues/video04.mp4 --configuration config.txt --results results/video_atrio_cues/video04.txt
-#python app/group07.py --video data/video_atrio_cues/video05.mp4 --configuration config.txt --results results/video_atrio_cues/video05.txt
-#python app/group07.py --video data/video_atrio_cues/video06.mp4 --configuration config.txt --results results/video_atrio_cues/video06.txt
-#python app/group07.py --video data/video_atrio_cues/video07.mp4 --configuration config.txt --results results/video_atrio_cues/video07.txt
-#python app/group07.py --video data/video_atrio_cues/video08.mp4 --configuration config.txt --results results/video_atrio_cues/video08.txt
-#python app/group07.py --video data/video_atrio_cues/video09.mp4 --configuration config.txt --results results/video_atrio_cues/video09.txt
-#python app/group07.py --video data/video_atrio_cues/video10.mp4 --configuration config.txt --results results/video_atrio_cues/video10.txt
-#python app/group07.py --video data/video_atrio_cues/video11.mp4 --configuration config.txt --results results/video_atrio_cues/video11.txt
-#python app/group07.py --video data/video_atrio_cues/video12.mp4 --configuration config.txt --results results/video_atrio_cues/video12.txt
-#python app/group07.py --video data/video_atrio_cues/video13.mp4 --configuration config.txt --results results/video_atrio_cues/video13.txt
-#python app/group07.py --video data/video_atrio_cues/video14.mp4 --configuration config.txt --results results/video_atrio_cues/video14.txt
-#python app/group07.py --video data/video_atrio_cues/video15.mp4 --configuration config.txt --results results/video_atrio_cues/video15.txt
-#python app/group07.py --video data/video_atrio_cues/video16.mp4 --configuration config.txt --results results/video_atrio_cues/video16.txt
-#python app/group07.py --video data/video_atrio_cues/video17.mp4 --configuration config.txt --results results/video_atrio_cues/video17.txt
-#python app/group07.py --video data/video_atrio_cues/video18.mp4 --configuration config.txt --results results/video_atrio_cues/video18.txt
-#python app/group07.py --video data/video_atrio_cues/video19.mp4 --configuration config.txt --results results/video_atrio_cues/video19.txt
-#python app/group07.py --video data/video_atrio_cues/video20.mp4 --configuration config.txt --results results/video_atrio_cues/video20.txt
-#python app/group07.py --video data/video_atrio_cues/video21.mp4 --configuration config.txt --results results/video_atrio_cues/video21.txt
-#python app/group07.py --video data/video_atrio_cues/video22.mp4 --configuration config.txt --results results/video_atrio_cues/video22.txt
-#python app/group07.py --video data/video_atrio_cues/video23.mp4 --configuration config.txt --results results/video_atrio_cues/video23.txt
-#python app/group07.py --video data/video_atrio_cues/video24.mp4 --configuration config.txt --results results/video_atrio_cues/video24.txt
-
-#python app/group07.py --video data/colors/black.mp4 --configuration config.txt --results results/colors/black.txt
-#python app/group07.py --video data/colors/blue.mp4 --configuration config.txt --results results/colors/blue.txt
-#python app/group07.py --video data/colors/brown_purple.mp4 --configuration config.txt --results results/colors/brown_purple.txt
-#python app/group07.py --video data/colors/gray.mp4 --configuration config.txt --results results/colors/gray.txt
-#python app/group07.py --video data/colors/green.mp4 --configuration config.txt --results results/colors/green.txt
-#python app/group07.py --video data/colors/pink.mp4 --configuration config.txt --results results/colors/pink.txt
-#python app/group07.py --video data/colors/red.mp4 --configuration config.txt --results results/colors/red.txt
-#python app/group07.py --video data/colors/white.mp4 --configuration config.txt --results results/colors/white.txt
-#python app/group07.py --video data/colors/yellow.mp4 --configuration config.txt --results results/colors/yellow.txt
-
-
 import argparse
 from tracking import ObjectTracker
 from ultralytics.utils import LOGGER
